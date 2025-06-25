042f585cd5dc42cfd63e0ca50242974c17e53623
pegonsLog <pegons.log@gmail.com>
2025-06-25 11:18:37 -0300

    Primeiro commit

---
+++ src/app/components/ci/ci.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ci',
+  standalone: true,
+  imports: [],
+  templateUrl: './ci.component.html',
+  styleUrl: './ci.component.scss'
+})
+export class CiComponent {
+
+}
